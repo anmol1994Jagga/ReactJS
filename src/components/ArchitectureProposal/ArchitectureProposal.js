17dbfe398e68aa8afdaade2c3624e9868d85a9ec
AnmolJagga <Anmol13jagga@gmail.com>
2024-08-04 18:28:53 +0530

    Architecture proposal link

---
+++ src/components/ArchitectureProposal/ArchitectureProposal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const ArchitectureProposal = () => {
+    return (
+      <div className="flex flex-col items-center min-h-screen">
+        <h1 className="text-2xl font-bold">Welcome to the Architecture Page</h1>
+        <a className="mt-4 text-blue-500 hover:underline text-2xl" href='docs/ARCHITECTURE_PROPOSAL.docx' download> Click here</a>
+      </div>
+    );
+  };
+export default ArchitectureProposal;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Home = () => {
+    return (
+      <div className="flex flex-col items-center min-h-screen">
+        <h1 className="text-2xl font-bold">Welcome to the App</h1>
+        <p className="mt-4">Select a feature from the navigation menu above.</p>
+      </div>
+    );
+  };
+export default Home;
